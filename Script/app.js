e94fb56470adfcb18137016bf4b5489ba264423f
pbuitron <pbuitron@users.noreply.github.com>
2023-02-01 20:46:34 -0500

    Avance Página inicial 80%

---
+++ Script/app.js
@@ -0,0 +1,6 @@
+window.addEventListener("load", function() {
+    document.querySelector(".loader-container").style.display = "none";
+    document.querySelector(".content").style.display = "block";
+  });
+
+  
