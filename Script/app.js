2d8efbc5b3b6658f79f1ec0317c7e0b48a72f061
pbuitron <pbuitron@users.noreply.github.com>
2023-02-02 19:24:41 -0500

    1ra seccion 85%

--- Script/app.js
+++ Script/app.js
@@ -1,6 +1,53 @@
-window.addEventListener("load", function() {
-    document.querySelector(".loader-container").style.display = "none";
-    document.querySelector(".content").style.display = "block";
-  });
 
+
+window.addEventListener("load", function () {
+  document.querySelector(".loader-container").style.display = "none";
+  document.querySelector(".content").style.display = "block";
+});
+
+class Cards {
+  constructor (nombre, descripcion, img){
+    this.nombre=nombre
+    this.descripcion=descripcion
+    this.img=img
+  }
+}
+
+
+
+let ArrayCards = [];
+
+  let card1 = new Cards(`Decadas de Experiencia`,`Somos Lideres globales en el transporte de desechos`,`./image/iconos/icon-1.webp` )
+  let card2 = new Cards( `Equipo Profesional`,`Somos Lideres globales en el transporte de desechos`,`./image/iconos/icon-2.webp` )
+  let card3 = new Cards(`Amamos los desafios`,`Somos Lideres globales en el transporte de desechos`,`./image/iconos/icon-3.webp`)
+  let card4 = new Cards(`Amplio Portfolio de Clientes`,`Somos Lideres globales en el transporte de desechos`,`./image/iconos/icon-4.webp`)
   
+  ArrayCards.push(card1)
+  ArrayCards.push(card2)
+  ArrayCards.push(card3)
+  ArrayCards.push(card4)
+  
+
+
+const contenedorPadre = document.getElementById(`sh2`)
+
+ArrayCards.forEach(tarjeta =>
+  {
+  contenedorPadre.innerHTML += `
+
+  <div class="col">
+    <div class="card">
+      <img src="${tarjeta.img}" class="card-img-top card1" alt="...">
+      <div class="card-body">
+        <h5 class="card-title fs-1 fw-bolder">${tarjeta.nombre}</h5>
+        <p class="card-text">${tarjeta.descripcion}</p>
+      </div>
+    </div>
+  </div>
+  `
+ 
+})
+
+
+
+
